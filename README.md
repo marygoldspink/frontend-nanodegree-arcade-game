cb4fd3fe19ff17837b25855756e64e895e793ee7
Mary Goldspink <marygoldspink@gmail.com>
2018-08-18 22:57:26 +0100

    My implementation of Frogger game

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+An implementation of Frogger in JavaScript for the Udacity Web Developer Nano Degree.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+## How to run
+
+To run simply open the index.html file in a browser. 
+
+The aim of the game is to move you player safely across the road to the water. If you reach the water without being hit by a vehicle then you win and the game resets itself.
+
+You can move your player using the arrow keys.
--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+// Parameter: startingRow, the row this enemy will move along
+// Parameter: speed, the speed at which this enemy will move
+var Enemy = function(startingRow, speed) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    // always start off the screen
+    this.x = -101;
+
+    // calculate the starting row
+    this.y = 50 + ((startingRow - 1) * 83);
+
+    // each enemy can have its own speed
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +22,28 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * 101 * dt;
+
+    // If we've reached the end of the screen 
+    // move the enemy back to the start
+    if (this.x > 505) {
+        this.x = -101;
+    }
+
+    // detect collision
+    // get the x and y position of our player
+    var playerY = player.getY(),
+        playerX = player.getX();
+    
+    // to see if we hit a player see if it's:
+    // - the same y position
+    // - the x position of this enemy is between the edges of the square the player is in.
+    if (playerY == this.y &&
+        this.x < playerX + 50 &&
+        this.x > playerX - 50) {
+        // collision so reset the player
+        player.reset();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +51,91 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// The player class
+var Player = function() {
+    this.sprite = 'images/char-princess-girl.png';
 
+    // for x & y I decided to use the row and column number,
+    // unlike the Enemy.
+    this.x = 2;
+    this.y = 5;
+}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// update function - does nothing because the movement
+// is handled in handleInput.
+Player.prototype.update = function() {
 
+};
 
+// handleInput function which moves the player based on the key pressed
+// Parameter: allowedKeys, the key which was pressed.
+Player.prototype.handleInput = function(allowedKeys) {
+    switch (allowedKeys) {
+        case 'up':      this.setY(-1);
+            break;
+        case 'down':    this.setY(1);
+            break;
+        case 'left':    this.setX(-1);
+            break;
+        case 'right':   this.setX(1);
+            break;
+    }
+};
+
+// setX moves the player in the x direction by the given increment.
+// We handle the edge of the screen in this function so the player 
+// will never appear off screen.
+// Parameter: xIncrement, the number of columns to move by.
+Player.prototype.setX = function(xIncrement) {
+    this.x += xIncrement;
+    if (this.x < 0) {
+        this.x = 0;
+    } else if (this.x > 4) {
+        this.x = 4;
+    }
+}
+
+// setY moves the player in the y direction by the given increment.
+// We handle the bottom of the screen in this function so the player 
+// will never appear off screen. If the player reaches the water
+// then the player has won and we reset the game.
+// Parameter: yIncrement, the number of rows to move by.
+Player.prototype.setY = function(yIncrement) {
+    this.y += yIncrement;
+    if (this.y == 0) {
+        this.reset();
+    } else if (this.y > 5) {
+        this.y = 5;
+    }
+}
+
+// getY calculates the y pixel position based on the current
+// y row number.
+Player.prototype.getY = function() {
+    return 50 + ((this.y - 1) * 83);
+}
+
+// getX calculates the x pixel position based on the current
+// x row number.
+Player.prototype.getX = function() {
+    return this.x * 101;
+}
+
+// reset's the player back to the starting point in the grid.
+Player.prototype.reset = function() {
+    this.x = 2;
+    this.y = 5;
+}
+
+// render the player on the grid 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.getX(), this.getY());
+};
+
+// initialize the enemies in different rows and with different speeds
+var allEnemies = [new Enemy(1, 1.5), new Enemy(3, 2), new Enemy(2, 2.5), new Enemy(1, 1) , new Enemy(2, 1.2)];
+// this is our player
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
